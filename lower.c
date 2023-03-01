87752d50457062da291fb209ce0cde05c26687c2
RASHIKA K V <107302130+rashika-0212@users.noreply.github.com>
2023-02-28 21:57:00 -0800

    Create lower.c

---
+++ lower.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+//#define r 3
+//#define c 3
+
+int main()
+{
+    int r,c;
+    printf("Enter the no of rows : ");
+    scanf("%d",&r);
+    printf("Enter the no of columns : ");
+    scanf("%d",&c);
+    int array[r][c];
+    int row, col, upper=0;
+    printf("Enter elements in matrix of size %dx%d: \n", r,c);
+    for(row=0;row<r;row++){
+        for(col=0;col<c;col++){
+            scanf("%d",&array[row][col]);
+        }
+    }
+    for(row=0; row<r; row++){
+        for(col=0; col<c; col++){
+            if(col>row && array[row][col]!=0){
+                upper = 1;
+            }
+        }
+    }
+    if(upper == 0){
+        printf("\nThe matrix is lower triangular matrix.\n");
+    }
+    else{
+        printf("\nThe matrix is not lower triangular matrix.");
+    }return 0;
+}
